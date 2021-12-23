1dc695fb95cfbb84c6bab41059e3aa923cd853c8
anthonysilva98 <anthonyssilva98@gmail.com>
2021-12-23 10:11:42 -0800

    Price container with name and percentage change

--- src/Screens/CoinDetailScreen/CoinDetailScreen.jsx
+++ src/Screens/CoinDetailScreen/CoinDetailScreen.jsx
@@ -3,21 +3,59 @@ import { View, Text, Image } from "react-native";
 import Coin from "../../../data/crypto.json";
 import styles from "./styles";
 import CoinDetailHeader from "./CoinDetailHeader";
+import { AntDesign } from "@expo/vector-icons";
 const CoinDetailScreen = () => {
+  const percentageChange =
+    price_change_percentage_24h < 0 ? "#16c784" : "#ea3943";
   const {
     image: { small },
     name,
     symbol,
-    market_data: { market_cap_rank },
+    market_data: {
+      market_cap_rank,
+      current_price,
+      price_change_percentage_24h,
+    },
   } = Coin;
+
   return (
-    <View>
+    // Coin Detail Header Section
+    <View style={{ paddingHorizontal: 10, paddingTop: 20 }}>
       <CoinDetailHeader
         image={small}
         name={name}
         symbol={symbol}
         marketCapRank={market_cap_rank}
       />
+      {/* Price Container with name Container */}
+      <View style={styles.priceContainer}>
+        <View>
+          <Text style={{ color: "white", fontSize: 15 }}> {name}</Text>
+          <Text style={styles.currentPrice}>${current_price.usd}</Text>
+        </View>
+        <View style={{ alignSelf: "center" }}>
+          {/* Show Percentage change with container */}
+          <View
+            style={{
+              backgroundColor: percentageChange,
+              flexDirection: "row",
+              padding: 5,
+              borderRadius: 5,
+              alignItems: "center",
+            }}
+          >
+            <AntDesign
+              name={price_change_percentage_24h < 0 ? "caretdown" : "caretup"}
+              size={15}
+              color={"white"}
+              style={{ alignSelf: "center", marginRight: 5 }}
+            />
+            <Text style={styles.priceChange}>
+              {price_change_percentage_24h.toFixed(2)}%
+            </Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
--- src/Screens/CoinDetailScreen/styles.jsx
+++ src/Screens/CoinDetailScreen/styles.jsx
@@ -23,6 +23,22 @@ const styles = StyleSheet.create({
     paddingVertical: 2,
     borderRadius: 5,
   },
+  currentPrice: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "600",
+    letterSpacing: 1,
+  },
+  priceContainer: {
+    padding: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  priceChange: {
+    color: "white",
+    fontSize: 17,
+    fontWeight: "600",
+  },
 });
 
 export default styles;
